d40aac6697f8a1100bf5b6a8e1c0e54f042fcc53
Elizabeth Wong <lizzwong@gmail.com>
2018-05-01 16:39:50 -0500

    views

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,17 +4,21 @@ import Header from '../Header/Header';
 import Stars from '../../pages/Stars/Stars';
 import Planets from '../../pages/Planets/Planets';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 class App extends Component {
 
 
 
   render() {
     return (
+      <Router>
       <div className="App">
         <Header title="React Astronomy"/>
-        <Stars/>
-        <Planets/>
+        <Route path="/stars" component={Stars}/>
+        <Route path="/planets" component={Planets}/>
       </div>
+      </Router>
     );
   }
 }
